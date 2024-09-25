fb249128f9e7db70c939d40769cb3664617f3db9
odinakahub <odinakahub@gmail.com>
2024-09-25 13:24:29 +0100

    Add files via upload

---
+++ navnav/index.js
@@ -0,0 +1,9 @@
+function showSubMenu(){
+	document.getElementById("navbar").style.display = "block";
+	document.getElementById("main-navbar").style.display = "none";
+	
+}
+function dontShowSubMenu(){
+	document.getElementById("navbar").style.display = "none";
+	document.getElementById("main-navbar").style.display = "block";
+}
