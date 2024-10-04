c23450830e9085d620f41368eaef0b8e41ca3882
odinakahub <odinakahub@gmail.com>
2024-10-04 17:10:28 +0100

    Add files via upload

---
+++ SAMPLENAV/sampleNav/index.js
@@ -0,0 +1,11 @@
+function showNav(){
+   document.getElementById("side-bar").style.display ="block"
+   document.getElementById("open").style.display ="none"
+   document.getElementById("close").style.display ="block"
+    
+}
+function closeNav(){
+document.getElementById("side-bar").style.display ="none"
+   document.getElementById("open").style.display ="block"
+   document.getElementById("close").style.display ="none"
+}
